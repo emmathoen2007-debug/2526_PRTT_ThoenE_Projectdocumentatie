de48e8afd745e02379f5baaae5dad417684b7c5c
emmathoen2007-debug <emmathoen2007@gmail.com>
2026-01-01 17:17:32 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",      // HTML in root
+    "./html/**/*.html" // HTML in submap en eventueel dieper
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
